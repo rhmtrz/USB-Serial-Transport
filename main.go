aa9641b0f3fc958953c73cf1f59e093b0aa88bb6
rhmtrz <rezaeihazara@gmail.com>
2023-11-13 23:11:10 +0900

    feat: publish and subscribe mqtt

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"os/signal"
 
-	model "example/rhmtrz/txrx-mqtt/pkg/model"
-	"example/rhmtrz/txrx-mqtt/pkg/serialPort"
-
+	model "github.com/adawarp/go-signal-transceiver/pkg/model"
+	"github.com/adawarp/go-signal-transceiver/pkg/mqttConnection"
+	"github.com/adawarp/go-signal-transceiver/pkg/serialPort"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
@@ -23,14 +25,34 @@ func init() {
 func main() {
 	flag.Parse()
 	if *version {
-		log.Println("Version: 1.0.0")
+		log.Println("Version: 1.1")
 		return
 	}
-
-	km := model.KeyManager()
+	km := model.KeyManeger()
 	keys := km.GetKeys()
 	msgChan := make(chan mqtt.Message)
 	serial := serialPort.NewSerial()
 	serial.Connect(keys.BaudRate)
 
+	clientManeger := mqttConnection.ClientManeger()
+	cl := clientManeger.MqttConnect(keys.MqttURL, keys.DestinationId, msgChan)
+
+	go clientManeger.PublishMqtt(keys.MyId, serial, *isLogging)
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+	for {
+		select {
+		case m := <-msgChan:
+			if *isLogging {
+				log.Println("sub: ", m.Payload(), m.Topic)
+			}
+			serial.Write(m.Payload())
+		case <-signalCh:
+			log.Println("!!!!!!!!!!!!")
+			log.Printf("Interrupt detected.\n")
+			cl.Disconnect(1000)
+			return
+		}
+	}
 }
